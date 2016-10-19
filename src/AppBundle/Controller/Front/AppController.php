3f722276f4d76c669e02589a8abec356a4c97f15
Julian <julian.michau@gmail.com>
2016-10-19 12:09:03 +0200

    index

--- src/AppBundle/Controller/Front/AppController.php
+++ src/AppBundle/Controller/Front/AppController.php
@@ -1,20 +1,28 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace AppBundle\Controller\Front;
+
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+
+/**
+ * Created by PhpStorm.
+ * User: Julian
+ * Date: 18/10/2016
+ * Time: 18:00
+ */
+class AppController extends Controller
 {
     /**
-     * @Route("/test", name="homepage")
+     * @Route("/", name="index")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('front/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
     }
