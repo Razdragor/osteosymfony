8f89ac000dcb91cb5261ef715618435942d8190c
Julian <julian.michau@gmail.com>
2016-11-29 16:35:48 +0100

    add crud user

---
+++ src/AppBundle/Controller/Front/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Controller\Front;
+
+use AppBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * User controller.
+ *
+ * @Route("/patient/user")
+ */
+class UserController extends Controller
+{
+    /**
+     * Lists all user entities.
+     *
+     * @Route("/", name="user_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $users = $em->getRepository('AppBundle:User')->findAll();
+
+        return $this->render('front/user/index.html.twig', array(
+            'users' => $users,
+        ));
+    }
+
+    /**
+     * Finds and displays a user entity.
+     *
+     * @Route("/{id}", name="user_show")
+     * @Method("GET")
+     */
+    public function showAction(User $user)
+    {
+
+        return $this->render('front/user/show.html.twig', array(
+            'user' => $user,
+        ));
+    }
+}
